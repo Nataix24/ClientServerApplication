362005d3fb2bb7d4f4b5f1463aef12dbd8457b70
OistinR <oistinrutledge1@gmail.com>
2023-11-15 17:43:53 +0100

    added blank template methods for pushing

--- src/main/java/compsec/clientserverapplication/demo.java
+++ src/main/java/compsec/clientserverapplication/demo.java
@@ -1,7 +1,9 @@
 package compsec.clientserverapplication;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +16,26 @@ public class demo {
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
+    }
+    @PostMapping("/signup")
+    public int signUp(@RequestParam(value = "id") String id,@RequestParam(value = "password") String password) {
+
+        //logic
+
+        return HttpServletResponse.SC_ACCEPTED;
+    }
+    @PostMapping("/signin")
+    public int signIn(@RequestParam(value = "id") String id,@RequestParam(value = "password") String password) {
+
+        //logic
+
+        return HttpServletResponse.SC_ACCEPTED;
+    }
+    @PostMapping("/counter")
+    public int counter(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+        //logic
+
+        return HttpServletResponse.SC_ACCEPTED;
     }
 }
