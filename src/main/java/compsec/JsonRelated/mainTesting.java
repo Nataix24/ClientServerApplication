410d52c7c97c7bdff31929baeed3b9b3f9f0ff22
Nataix24 <czbn24@gmail.com>
2023-11-17 18:07:27 +0100

    Json file reade completed

---
+++ src/main/java/compsec/JsonRelated/mainTesting.java
@@ -0,0 +1,19 @@
+package compsec.JsonRelated;
+
+import java.util.HashMap;
+
+public class mainTesting {
+    public static void main(String[] args) {
+        //testing file reading
+        JsonLogics readJson = new JsonLogics();
+        HashMap<String,String> stringHashMap = readJson.readFile();
+        System.out.println(stringHashMap.get("password"));
+        System.out.println(stringHashMap.get("id"));
+        System.out.println(stringHashMap.get("ip"));
+        System.out.println(stringHashMap.get("port"));
+        System.out.println(stringHashMap.get("delay"));
+        System.out.println(stringHashMap.get("action1"));
+        System.out.println(stringHashMap.get("action2"));
+        System.out.println(stringHashMap.get("action3"));
+    }
+}
