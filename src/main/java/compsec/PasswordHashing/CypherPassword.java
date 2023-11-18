4f1c211512afb1737514c8bab7aceeebd4f69200
Illarionis <olexander.finkov@gmail.com>
2023-11-18 15:05:46 +0100

    CypherPassword
    To use:
    
    A. To hash a password:
    1. Create instance obj of CypherPassword, in constructor pass the raw password String, the constructor then hashes the password and obj stores the values you may need
    2. To get salt, use obj.getSalt()
    3. To get the hashed password, use obj.getHashedPassword()
    
    B. To verify a password, use the static method verifyPassword(rawPassword, hashedPassword, salt)

---
+++ src/main/java/compsec/PasswordHashing/CypherPassword.java
@@ -0,0 +1,87 @@
+package compsec.PasswordHashing;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+public class CypherPassword {
+
+    // Pepper is global, should be static
+    private static final String PEPPER = "R3d_H0t_Ch1ll1_P3pp3R";
+
+    private byte[] salt;
+    private String hashedPassword;
+
+    public CypherPassword(String rawPassword){
+        this.salt = generateSalt();
+        this.hashedPassword = hash(rawPassword, this.salt);
+    }
+    public byte[] getSalt(){
+        return this.salt;
+    }
+    public String getHashedPassword(){
+        return this.hashedPassword;
+    }
+
+    // Utility, convert byte[] to String
+    public static String bytesToString(byte[] bytes) {
+        StringBuilder result = new StringBuilder();
+        for (byte aByte : bytes) {
+            result.append(String.format("%02x", aByte));
+        }
+        return result.toString();
+    }
+
+    public static byte[] generateSalt() {
+        SecureRandom random = new SecureRandom();
+        byte[] saltArr = new byte[16];
+        random.nextBytes(saltArr);
+        return saltArr;
+    }
+
+
+    // Hash the password given salt
+    public static String hash(String rawPassword, byte[] salt) {
+        try {
+            rawPassword += PEPPER;
+            KeySpec spec = new PBEKeySpec(rawPassword.toCharArray(), salt, 65536, 128);
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");;
+            byte[] hash = factory.generateSecret(spec).getEncoded();
+            return bytesToString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Oopsie with getting algorithm instance 0_0");
+            e.printStackTrace();
+            return "ERROR_1";
+        } catch (InvalidKeySpecException e) {
+            System.out.println("Oopsie with generating the hashed password 0_0");
+            e.printStackTrace();
+            return "ERROR_2";
+        }
+    }
+
+    // Compare a raw String to a hashed password
+    public static boolean verifyPassword(String rawPassword, String hashedPassword, byte[] salt) {
+        String newHash = hash(rawPassword, salt);
+        return hashedPassword.equals(newHash);
+    }
+
+    public static void main(String[] args) {
+        String originalRaw = "MyCoolPasswordHaha";
+
+        CypherPassword cypher = new CypherPassword(originalRaw);
+        byte[] salt = cypher.getSalt();
+        String hashedOriginal = cypher.getHashedPassword();
+        
+        String testRaw = "MyCoolPasswordHaha";
+        boolean match = verifyPassword(testRaw, hashedOriginal, salt);
+
+        System.out.println("Raw stored:     " + originalRaw);
+        System.out.println("Raw input:      " + testRaw);
+        System.out.println("Hashed stored:  " + hashedOriginal);
+        System.out.println("Match:          " + match);
+    }
+}
