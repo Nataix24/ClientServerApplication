677f22d2fc4f82b2303b46a4ed6c57e84689337c
j0sh162 <joshkramer567@gmail.com>
2023-11-19 22:57:15 +0100

    client with hashing

--- src/main/java/compsec/client/ClientLogics.java
+++ src/main/java/compsec/client/ClientLogics.java
@@ -1,124 +1,75 @@
 package compsec.client;
 
+import compsec.PasswordHashing.HashingLogics;
 import compsec.clientserverapplication.JsonLogics;
 import compsec.clientserverapplication.ServerLogics;
+
+import org.json.simple.parser.ParseException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 
 
 public class ClientLogics {
+    public static void main(String[] args) throws FileNotFoundException, ParseException, InterruptedException, ExecutionException {
+        boolean exit = false;
+        while(!exit){
 
-    public static void main(String[] args) {
-//        SpringApplication.run(ServerLogics.class, args);
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter the path to the JSON client file:");
-        String filePath = myObj.nextLine();
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter the path to the JSON client file or type exit:");
+            String file = input.nextLine();
+            input.close();
+            if(file.equals("exit")){
+                exit= true;
+            }else{
+                File myObj = new File(file);
+                Scanner myReader = new Scanner(myObj);
+                String data = "";
+                while (myReader.hasNextLine()) {
+                   data += myReader.nextLine(); 
+                }
+                myReader.close();
+                String[] splitData= data.split("\"");
+                for (int i = 0; i < splitData.length; i++) {
+                    if(splitData[i].equals("password")){
+                        int index = i+2;
+                        HashingLogics hash = new HashingLogics(splitData[index]);
+                        splitData[index] = hash.getHashedPassword();
+                    }
+                }
+                data = "";
+                for (int i = 0; i < splitData.length; i++) {
+                    data += splitData[i];
+                }
+
+                JsonLogics parser = new JsonLogics();
+                HashMap<String,String> parsedData = parser.readFile(data);
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(parsedData.get("ip")+":"+parsedData.get("port")))
+                .POST(HttpRequest.BodyPublishers.ofString(data))
+                .build();
+                CompletableFuture<HttpResponse<String>> futureResponse = client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+                HttpResponse<String> response = futureResponse.get();
+                System.out.println(response.statusCode());
+            }
+        }
 //        start(filePath);
     }
-    //"src/main/java/compsec/clients/test.json"
-    // this is bad. but can be improved before deadline:https://www.baeldung.com/spring-boot-https-self-signed-certificate
-//    public static void start(String filePath){
-//        Scanner myObj = new Scanner(System.in);
-//
-//        JsonLogics readJson = new JsonLogics();
-//        HashMap clientData = readJson.readFile(filePath);
-//
-//        System.out.println(clientData.toString());
-//        System.out.println("Connecting to server...");
-//
-//        //check needs to be done for SERVER and PORT.
-//        if(!clientData.get("ip").toString().replace(" ","").equals("localhost")){
-//            System.out.println("Incorrect Server Connection");
-//            try {
-//                Thread.sleep(2000);
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-//            System.out.println("Press Enter to Exit.");
-//            myObj.nextLine();
-//            System.exit(1);
-//        }
-//        if(!clientData.get("port").toString().replace(" ","").equals("8080")){
-//            System.out.println("Incorrect PORT");
-//
-//            try {
-//                Thread.sleep(2000);
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-//            System.out.println("Press Enter to Exit.");
-//            myObj.nextLine();
-//            System.exit(1);
-//        }
-//
-//        System.out.println("Attempting Login..");
-//        WebClient webClient = WebClient.create();
-//        String responseBody = webClient.post()
-//                .uri("http://localhost:8080/login")
-//                .body(BodyInserters.fromFormData("id", clientData.get("id").toString().replace(" ",""))
-//                        .with("password", clientData.get("password").toString().replace(" ","")))
-//                .retrieve()
-//                .bodyToMono(String.class)
-//                .block();
-//
-//        // Print the response
-//        assert responseBody != null;
-//        if(responseBody.equals("200")){
-//            System.out.println("Success!");
-//        }
-//        else if(responseBody.equals("401")){
-//            System.out.println("Incorrect username or password. Would you like to make a new account? y/n");
-//            if(myObj.nextLine().equalsIgnoreCase("y")){
-//                signUp(webClient,clientData);
-//            }
-//            else {
-//                System.out.println("Press Enter to Exit.");
-//                myObj.nextLine();
-//                System.exit(0);
-//            }
-//        }
-//        else {
-//            System.out.println("Response: " + responseBody); //idk what to do? do we stop? depends on the response. this is for signing in.
-//        }
-//
-//        System.out.println("Signing Up..");
-//
-//
-//        // login then if account does not exist then sign up then login.
-//
-//        //clientData.get("ip").toString().replace(" ","")+":"+clientData.get("port").toString().replace(" ","")
-//
-//
-//        System.out.println("Connecting to Signing Up...");
-//
-//
-
-//    }
-
-//    private static void signUp(WebClient webClient, HashMap clientData){
-//        String responseBody2 = webClient.post()
-//                .uri("http://localhost:8080/signup")
-//                .body(BodyInserters.fromFormData("id", clientData.get("id").toString().replace(" ",""))
-//                        .with("password", clientData.get("password").toString().replace(" ","")))
-//                .retrieve()
-//                .bodyToMono(String.class)
-//                .block();
-//
-//        // Print the response
-//        assert responseBody2 != null;
-//        if(responseBody2.equals("200")){
-//            System.out.println("Success!");
-//        }
-//        else {
-//            System.out.println("Response: " + responseBody2);//idk what to do? do we stop? depends on the response. this is for signing in.
-//        }
-//    }
-//
 
 }
--- src/main/java/compsec/clientserverapplication/JsonLogics.java
+++ src/main/java/compsec/clientserverapplication/JsonLogics.java
@@ -2,50 +2,55 @@ package compsec.clientserverapplication;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import java.util.ArrayList;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class JsonLogics {
 
-    public Client readFile(String clientJson) {
+    public HashMap<String,String> readFile(String json) throws ParseException {
         HashMap<String, String> stringHashMap = new HashMap<>();
         String password = "";
         String id = "";
         String ip = "";
         String port = "";
         String delay = "";
-        ArrayList<String> actionsAL = new ArrayList<>();
-        Object obj = clientJson;
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(json);
+
         JSONObject jsonObject = (JSONObject) obj;
-        id = (String) jsonObject.get(" id ");
+        id = (String) jsonObject.get("id");
         stringHashMap.put("id", id);
-        password = (String) jsonObject.get(" password ");
+        password = (String) jsonObject.get("password");
         stringHashMap.put("password", password);
 
-        Object serverObject = jsonObject.get(" server ");
+        Object serverObject = jsonObject.get("server");
         JSONObject server = (JSONObject) serverObject;
         // Retrieve "ip" and "port" values
-        ip = (String) server.get(" ip ");
-        port = (String) server.get(" port ");
+        ip = (String) server.get("ip");
+        port = (String) server.get("port");
         stringHashMap.put("ip", ip);
         stringHashMap.put("port", port);
-        Object actionsObject = jsonObject.get(" actions ");
+        Object actionsObject = jsonObject.get("actions");
         JSONObject actions = (JSONObject) actionsObject;
         // Retrieve "ip" and "port" values
-        delay = (String) actions.get(" delay ");
+        delay = (String) actions.get("delay"); 
         stringHashMap.put("delay", delay);
         // Retrieve "steps" array
-        JSONArray stepsArray = (JSONArray) actions.get(" steps ");
+        JSONArray stepsArray = (JSONArray) actions.get("steps");
         // Process each step in the array
         int counter = 1;
         for (Object step : stepsArray) {
             String actionStep = (String) step;
-            actionsAL.add(actionStep);
             stringHashMap.put("action" + counter, actionStep);
             counter++;
         }
 
-        return new Client(stringHashMap.get("id"), stringHashMap.get("password"), stringHashMap.get("counter"),
-                stringHashMap.get("server"), stringHashMap.get("port"), actionsAL, stringHashMap.get("delay"));
+        return stringHashMap;
     }
+
 }
