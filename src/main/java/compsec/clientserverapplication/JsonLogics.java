8bbcbc928865e81a85ae2c27224bb94da3bb64f1
Ana Godorogea <anagdrgea@gmail.com>
2023-12-29 20:02:42 +0100

    throw exception of instruction does not contain integer

--- src/main/java/compsec/clientserverapplication/JsonLogics.java
+++ src/main/java/compsec/clientserverapplication/JsonLogics.java
@@ -50,9 +50,16 @@ public class JsonLogics {
         int counter = 1;
         for (Object step : stepsArray) {
             String actionStep = (String) step;
-            stringHashMap.put("action" + counter, actionStep);
-            stepsList.add(actionStep);
-            counter++;
+            try {
+                // validate if it's an integer
+                Integer.parseInt(actionStep.substring(9));
+                stringHashMap.put("action" + counter, actionStep);
+                stepsList.add(actionStep);
+                counter++;
+            }
+            catch(NumberFormatException e) {
+                System.out.println("You need to input an integer!");
+            }
         }
 
         return stringHashMap;
